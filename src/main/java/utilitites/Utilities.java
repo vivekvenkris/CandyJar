47c2d5b42fdec2b3035292f217e9f01c30c09e8d
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2020-11-24 15:48:03 +0100

    utility functions

--- src/main/java/utilitites/Utilities.java
+++ src/main/java/utilitites/Utilities.java
@@ -82,6 +82,9 @@ public class Utilities {
 	public static String getUTCString(LocalDateTime utcTime, String format){
 		return utcTime.format(DateTimeFormatter.ofPattern(format));
 	}
+	public static String getUTCString(LocalDateTime utcTime, DateTimeFormatter format){
+		return utcTime.format(format);
+	}
 	
 	public static String getUTCString(LocalDateTime utcTime){
 		return utcTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd kk:mm:ss"));
@@ -89,6 +92,9 @@ public class Utilities {
 	
 	public static LocalDateTime getUTCLocalDateTime(String utc, String pattern){
 		return LocalDateTime.parse(utc, DateTimeFormatter.ofPattern(pattern));
+	}
+	public static LocalDateTime getUTCLocalDateTime(String utc, DateTimeFormatter pattern){
+		return LocalDateTime.parse(utc, pattern);
 	}
 	
 
@@ -104,5 +110,53 @@ public class Utilities {
 		
 	}
 
+	
+//	public static WritableImage toFXImage(BufferedImage bimg, WritableImage wimg) {
+//		         int bw = bimg.getWidth();
+//		         int bh = bimg.getHeight();
+//		         switch (bimg.getType()) {
+//		             case BufferedImage.TYPE_INT_ARGB:
+//		          case BufferedImage.TYPE_INT_ARGB_PRE:
+//		             default:
+//		                 BufferedImage converted =
+//		                     new BufferedImage(bw, bh, BufferedImage.TYPE_INT_ARGB_PRE);
+//		                 Graphics2D g2d = converted.createGraphics();
+//		                 g2d.drawImage(bimg, 0, 0, null);
+//		                 g2d.dispose();
+//		                 bimg = converted;
+//		                 break;
+//		         }
+//		         // assert(bimg.getType == TYPE_INT_ARGB[_PRE]);
+//		         if (wimg != null) {
+//		             int iw = (int) wimg.getWidth();
+//		             int ih = (int) wimg.getHeight();
+//		             if (iw < bw || ih < bh) {
+//		                 wimg = null;
+//		             } else if (bw < iw || bh < ih) {
+//		                 int empty[] = new int[iw];
+//		                 PixelWriter pw = wimg.getPixelWriter();
+//		                 PixelFormat<IntBuffer> pf = PixelFormat.getIntArgbPreInstance();
+//		                 if (bw < iw) {
+//		                     pw.setPixels(bw, 0, iw-bw, bh, pf, empty, 0, 0);
+//		                 }
+//		                 if (bh < ih) {
+//		                     pw.setPixels(0, bh, iw, ih-bh, pf, empty, 0, 0);
+//		                 }
+//		             }
+//		         }
+//		         if (wimg == null) {
+//		             wimg = new WritableImage(bw, bh);
+//		         }
+//		         PixelWriter pw = wimg.getPixelWriter();
+//		         IntegerComponentRaster icr = (IntegerComponentRaster) bimg.getRaster();
+//		         int data[] = icr.getDataStorage();
+//		         int offset = icr.getDataOffset(0);
+//		         int scan = icr.getScanlineStride();
+//		         PixelFormat<IntBuffer> pf = (bimg.isAlphaPremultiplied() ?
+//		                                      PixelFormat.getIntArgbPreInstance() :
+//		                                      PixelFormat.getIntArgbInstance());
+//		         pw.setPixels(0, 0, bw, bh, pf, data, offset, scan);
+//		         return wimg;
+//		     }
 
 }
