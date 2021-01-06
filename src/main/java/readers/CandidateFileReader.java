fc885f3ddbef06ac670184931d84fa5c8cd73c03
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-01-06 12:55:00 +0100

    fixed imports

--- src/main/java/readers/CandidateFileReader.java
+++ src/main/java/readers/CandidateFileReader.java
@@ -3,7 +3,6 @@ package readers;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
