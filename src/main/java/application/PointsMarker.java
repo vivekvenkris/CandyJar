bd224879c229e98598b1460f432c409d68f5578c
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-03-17 17:36:14 +0100

    Alpha version of the  zoom and filter logic.

--- src/main/java/application/PointsMarker.java
+++ src/main/java/application/PointsMarker.java
@@ -1,31 +1,35 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 import org.controlsfx.glyphfont.Glyph;
 
-import de.gsi.chart.axes.Axis;
+import data_holders.Candidate.CANDIDATE_TYPE;
 import de.gsi.chart.axes.AxisMode;
 import de.gsi.chart.plugins.Zoomer;
-import de.gsi.chart.plugins.Zoomer.ZoomState;
-import javafx.collections.ListChangeListener;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Separator;
 import javafx.scene.control.Tooltip;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 
 public class PointsMarker extends Zoomer{ 
     private static final String FONT_AWESOME = "FontAwesome";
     private static final int FONT_SIZE = 20;
 
-    private  Button shortlistButton; 
-    private  Button filterButton;
-    private Button resetButton;
+    private Button shortlistButton; 
+    private Button filterButton;
+    private Button shortlistResetButton;
+    private Button filterResetButton;
+    
+	private ComboBox<CANDIDATE_TYPE> classifyBox;
 
     public PointsMarker() {
     	this(AxisMode.XY, false);
@@ -40,19 +44,24 @@ public class PointsMarker extends Zoomer{
     	super(zoomMode, animated);
     }
     
-    @Override
-    public HBox getZoomInteractorBar() {
-    	
-    	/* had to initialise these here because this function is called when constructing the super class */ 
-    	
+    public HBox getCustomToolBar() {
     	shortlistButton = new Button("mark",new Glyph(FONT_AWESOME, "\uf022").size(FONT_SIZE));
         filterButton  = new Button("filter",  new Glyph(FONT_AWESOME, "\uf0b0").size(FONT_SIZE));
-        resetButton = new Button("reset",  new Glyph(FONT_AWESOME, "\uf071").size(FONT_SIZE));
+        shortlistResetButton = new Button("",  new Glyph(FONT_AWESOME, "\uf28d").size(FONT_SIZE));
+        filterResetButton = new Button("",  new Glyph(FONT_AWESOME, "\uf28d").size(FONT_SIZE));
+        classifyBox = new ComboBox<CANDIDATE_TYPE>(FXCollections.observableArrayList(Arrays.asList(CANDIDATE_TYPE.values())));
+        classifyBox.setPromptText("CLASSIFY_AS");
     	HBox buttonBar = super.getZoomInteractorBar();
 
     	if(shortlistButton == null) {
     		return buttonBar;
     	}
+    	Separator separator1 = new Separator();
+    	separator1.setOrientation(Orientation.VERTICAL);    	
+    	Separator separator2 = new Separator();
+    	separator1.setOrientation(Orientation.VERTICAL);    	
+    	Separator separator3 = new Separator();
+    	separator1.setOrientation(Orientation.VERTICAL);
     	
         shortlistButton.setPadding(new Insets(3, 3, 3, 3));
         shortlistButton.setTooltip(new Tooltip("Mark observations with a different symbol"));
@@ -60,13 +69,24 @@ public class PointsMarker extends Zoomer{
         filterButton.setPadding(new Insets(3, 3, 3, 3));
         filterButton.setTooltip(new Tooltip("Filter only these candidates"));
 
-        resetButton.setPadding(new Insets(3, 3, 3, 3));
-        resetButton.setTooltip(new Tooltip("Reset filters and markers"));
+        shortlistResetButton.setPadding(new Insets(3, 3, 3, 3));
+        shortlistResetButton.setTooltip(new Tooltip("Reset markers"));
         
-        buttonBar.getChildren().addAll(shortlistButton, resetButton);
- 
+        filterResetButton.setPadding(new Insets(3, 3, 3, 3));
+        filterResetButton.setTooltip(new Tooltip("Reset filters"));
+        
+        return new HBox(separator1, shortlistButton, shortlistResetButton, separator2, filterButton, filterResetButton, separator3, classifyBox);
+    }
+    
+    @Override
+    public HBox getZoomInteractorBar() {
+
+        HBox buttonBar = super.getZoomInteractorBar();
+        buttonBar.getChildren().addAll(getCustomToolBar().getChildren());
     	return buttonBar;
     }
+    
+    
     
     
 	public Button getShortlistButton() {
@@ -81,11 +101,20 @@ public class PointsMarker extends Zoomer{
 
 	
 
-	public Button getResetButton() {
-		return resetButton;
+	public Button getShortlistResetButton() {
+		return shortlistResetButton;
+	}
+	
+	public Button getFilterResetButton() {
+		return filterResetButton;
 	}
 
-
+	
+	
+	public ComboBox<CANDIDATE_TYPE> getClassifyBox(){
+		return classifyBox;
+	}
+	
 //    private void registerZoomerChangeListener(final Zoomer zoomer) {
 //    	
 //
