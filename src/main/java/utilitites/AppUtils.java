896078f8543c0f63563bcd84bf458afc5d59fb10
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-06-12 13:30:52 +0200

    Added routines for clipboard manipulation

--- src/main/java/utilitites/AppUtils.java
+++ src/main/java/utilitites/AppUtils.java
@@ -2,13 +2,21 @@ package utilitites;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 
 import data_holders.Angle;
 import de.gsi.chart.marker.Marker;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 
@@ -50,6 +58,52 @@ public interface AppUtils {
 	};
 	
 	
+	public static void copyToClipboardText(String s) {
+
+		final Clipboard clipboard = Clipboard.getSystemClipboard();
+		final ClipboardContent content = new ClipboardContent();
+
+		content.putString(s);
+		clipboard.setContent(content);
+
+	}
+
+	public static void copyToClipboardImage(Label lbl) {
+
+		WritableImage snapshot = lbl.snapshot(new SnapshotParameters(), null);
+		final Clipboard clipboard = Clipboard.getSystemClipboard();
+		final ClipboardContent content = new ClipboardContent();
+
+		content.putImage(snapshot);
+		clipboard.setContent(content);
+
+	}
+
+	public static void copyToClipboardImageFromFile(String path) {
+
+		final Clipboard clipboard = Clipboard.getSystemClipboard();
+		final ClipboardContent content = new ClipboardContent();
+
+		content.putImage(AppUtils.getImage(path));
+		clipboard.setContent(content);
+
+	}
+	
+	public static ImageView setIcon(String path) {
+
+		InputStream is = AppUtils.class.getResourceAsStream(path);
+		ImageView iv = new ImageView(new Image(is));
+
+		iv.setFitWidth(100);
+		iv.setFitHeight(100);
+		return iv;
+	}
+	
+	public static Image getImage(String path) {
+
+		InputStream is = AppUtils.class.getResourceAsStream(path);
+		return new Image(is);
+	}
 	
 	
 	Callback<ListView<String>, ListCell<String>> fileNameViewFactory = new Callback<ListView<String>,ListCell<String>>(){
