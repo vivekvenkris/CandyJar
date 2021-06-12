63c4c076aef46c15d0685f4dadd15590117c548c
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-06-12 11:01:20 +0200

    Added routine to compute beam intersection, refactored find neighbours

--- src/main/java/data_holders/MetaFile.java
+++ src/main/java/data_holders/MetaFile.java
@@ -57,72 +57,53 @@ public class MetaFile {
 		return s;
 	}
 	
+	public static double getBeamIntersection(Beam b1, Beam b2) {
+		double xMean = b1.getRa().getRadianValue();
+		double yMean = b1.getDec().getRadianValue();
+		double x = b2.getRa().getRadianValue();
+		double y = b2.getDec().getRadianValue();				
+		double angle = b2.getEllipseConfig().getBeamAngle().getRadianValue() - Math.PI;
+		double xSigma = b2.getEllipseConfig().getBeamX().getRadianValue();
+		double ySigma = b2.getEllipseConfig().getBeamY().getRadianValue();
+		double a = Math.cos(angle) * Math.cos(angle) /  (2 * xSigma * xSigma)  + Math.sin(angle) * Math.sin(angle) / (2 * ySigma * ySigma);
+		double b  = - Math.sin(2 * angle) / (4 * xSigma * xSigma) + Math.sin(2 * angle) / (4 * ySigma * ySigma);
+		double c = Math.sin(angle) * Math.sin(angle) /  (2 * xSigma * xSigma)  + Math.cos(angle) * Math.cos(angle) / (2 * ySigma * ySigma);
+		double d = a  * Math.pow(x - xMean, 2) + 2 * b  * (x - xMean) * (y - yMean) + c * Math.pow(y - yMean, 2);
+		return  Math.exp(d);
+	}
+	
+
 	public void findNeighbours() {
 		
-//		for(Beam b1: beams.values()) {
-//			
-//			//System.err.println("Checking beam" + b1.getName());
-//			
-//			for(Beam b2: beams.values()) {
-//				
-//				
-//				
-//				Angle distance = Utilities.getAngularDistance(b1, b2);
-//				
-//				
-//				Angle theta = new Angle(b2.getEllipseConfig().getBeamAngle().getDegreeValue() - 90 + Utilities.getPositionAngle(b1, b2).getDegreeValue(), 
-//						Angle.DEG);
-//				
-//				Double r = Utilities.distanceFromEllipseCenter(b1.getEllipseConfig().getBeamY().getRadianValue(),
-//						b1.getEllipseConfig().getBeamX().getRadianValue(), theta.getRadianValue());
-//				
-////				System.err.println(b2.getName() + " " + distance.getDegreeValue() + " " 
-////				+ theta.getDegreeValue() + " " + r +  " " + distance.getRadianValue() / r  + " " + Utilities.isWithinEllipse(
-////						b1.getRa().getRadianValue(), b1.getDec().getRadianValue(), b2.getRa().getRadianValue(), b2.getDec().getRadianValue(),
-////						b1.getEllipseConfig().getBeamX().getRadianValue()*2, b1.getEllipseConfig().getBeamY().getRadianValue()*2));
-//			
-//				if (distance.getRadianValue() / r <= 2) {
-//					System.err.println(b2.getName() + " is a neighbour of" + b1.getName());
-//					//if (!b1.equals(b2))		b1.addNeighbour(b2);
-//				}
-//				
-//				
-//					
-//				
-//				
-//			}
-//			
-//			//System.err.println();
-//			
-//				
-//			
-//		}
-		
+
 		for(Beam b1: beams.values()) {
 			
-				//System.err.println("Checking beam" + b1.getName());
 				Map<Beam, Double> beamResponses = new HashMap<Beam, Double>();
 			
 			for(Beam b2: beams.values()) {
 				
 				if(b1.equals(b2)) continue;
 				
-				double xMean = b1.getRa().getRadianValue();
-				double yMean = b1.getDec().getRadianValue();
-				double x = b2.getRa().getRadianValue();
-				double y = b2.getDec().getRadianValue();				
-				double angle = b2.getEllipseConfig().getBeamAngle().getRadianValue() - Math.PI;
-				double xSigma = b2.getEllipseConfig().getBeamX().getRadianValue();
-				double ySigma = b2.getEllipseConfig().getBeamY().getRadianValue();
-				double a = Math.cos(angle) * Math.cos(angle) /  (2 * xSigma * xSigma)  + Math.sin(angle) * Math.sin(angle) / (2 * ySigma * ySigma);
-				double b  = - Math.sin(2 * angle) / (4 * xSigma * xSigma) + Math.sin(2 * angle) / (4 * ySigma * ySigma);
-				double c = Math.sin(angle) * Math.sin(angle) /  (2 * xSigma * xSigma)  + Math.cos(angle) * Math.cos(angle) / (2 * ySigma * ySigma);
-				double d = a  * Math.pow(x - xMean, 2) + 2 * b  * (x - xMean) * (y - yMean) + c * Math.pow(y - yMean, 2);
-				beamResponses.putIfAbsent(b2, Math.exp(d));
+//				double xMean = b1.getRa().getRadianValue();
+//				double yMean = b1.getDec().getRadianValue();
+//				double x = b2.getRa().getRadianValue();
+//				double y = b2.getDec().getRadianValue();				
+//				double angle = b2.getEllipseConfig().getBeamAngle().getRadianValue() - Math.PI;
+//				double xSigma = b2.getEllipseConfig().getBeamX().getRadianValue();
+//				double ySigma = b2.getEllipseConfig().getBeamY().getRadianValue();
+//				double a = Math.cos(angle) * Math.cos(angle) /  (2 * xSigma * xSigma)  + Math.sin(angle) * Math.sin(angle) / (2 * ySigma * ySigma);
+//				double b  = - Math.sin(2 * angle) / (4 * xSigma * xSigma) + Math.sin(2 * angle) / (4 * ySigma * ySigma);
+//				double c = Math.sin(angle) * Math.sin(angle) /  (2 * xSigma * xSigma)  + Math.cos(angle) * Math.cos(angle) / (2 * ySigma * ySigma);
+//				double d = a  * Math.pow(x - xMean, 2) + 2 * b  * (x - xMean) * (y - yMean) + c * Math.pow(y - yMean, 2);
+				beamResponses.putIfAbsent(b2, getBeamIntersection(b1, b2));
 				
 			}
 			
-		    b1.addNeighbours(beamResponses.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toList()).subList(0, 6).stream().map(f -> f.getKey()).collect(Collectors.toList()));
+		    b1.addNeighbours(
+		    		beamResponses.entrySet().stream()
+		    								.sorted(Map.Entry.comparingByValue())
+		    								.collect(Collectors.toList()).subList(0, 6).stream().map(f -> f.getKey())
+		    								.collect(Collectors.toList()));
 			
 		}
 		
@@ -250,7 +231,44 @@ public class MetaFile {
 	}
 	
 	
-	
-	
+	// old find neighbours
+//	for(Beam b1: beams.values()) {
+//	
+//	//System.err.println("Checking beam" + b1.getName());
+//	
+//	for(Beam b2: beams.values()) {
+//		
+//		
+//		
+//		Angle distance = Utilities.getAngularDistance(b1, b2);
+//		
+//		
+//		Angle theta = new Angle(b2.getEllipseConfig().getBeamAngle().getDegreeValue() - 90 + Utilities.getPositionAngle(b1, b2).getDegreeValue(), 
+//				Angle.DEG);
+//		
+//		Double r = Utilities.distanceFromEllipseCenter(b1.getEllipseConfig().getBeamY().getRadianValue(),
+//				b1.getEllipseConfig().getBeamX().getRadianValue(), theta.getRadianValue());
+//		
+////		System.err.println(b2.getName() + " " + distance.getDegreeValue() + " " 
+////		+ theta.getDegreeValue() + " " + r +  " " + distance.getRadianValue() / r  + " " + Utilities.isWithinEllipse(
+////				b1.getRa().getRadianValue(), b1.getDec().getRadianValue(), b2.getRa().getRadianValue(), b2.getDec().getRadianValue(),
+////				b1.getEllipseConfig().getBeamX().getRadianValue()*2, b1.getEllipseConfig().getBeamY().getRadianValue()*2));
+//	
+//		if (distance.getRadianValue() / r <= 2) {
+//			System.err.println(b2.getName() + " is a neighbour of" + b1.getName());
+//			//if (!b1.equals(b2))		b1.addNeighbour(b2);
+//		}
+//		
+//		
+//			
+//		
+//		
+//	}
+//	
+//	//System.err.println();
+//	
+//		
+//	
+//}
 
 }
