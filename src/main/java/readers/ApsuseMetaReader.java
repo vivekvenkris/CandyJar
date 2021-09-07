b1d5c4d466a890a9531d6179f74b665937841294
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-09-07 10:38:23 +0200

    Added sky to pixel conversion

--- src/main/java/readers/ApsuseMetaReader.java
+++ src/main/java/readers/ApsuseMetaReader.java
@@ -1,5 +1,6 @@
 package readers;
 
+import java.awt.geom.Point2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
@@ -17,6 +18,7 @@ import data_holders.Angle;
 import data_holders.Beam;
 import data_holders.EllipseConfig;
 import data_holders.MetaFile;
+import jsky.coords.WCSTransform;
 import utilitites.Utilities;
 
 
@@ -60,17 +62,40 @@ public class ApsuseMetaReader {
 			JsonReader beamShapeJsonReader = Json.createReader(new StringReader(beamShapeJson));			
 			JsonObject beamShapeJsonObject = beamShapeJsonReader.readObject();
 			
+			
+			
+			Beam boresight = new Beam(jsonObject.getJsonString("boresight").getString());
+			
+			metaFile.setBoresight(boresight);
+			
+			
+			double  step = 1/10000000000.;
+			WCSTransform wcstrans = new WCSTransform(
+					  boresight.getRa().getDegreeValue(), boresight.getDec().getDegreeValue(),
+					  -step * 3600,
+					  step * 3600,
+					  0,0, 
+					  200, 200,
+					  0,
+					  2000, 2000.0,
+					  "-TAN");
+			
+			
+			
 			EllipseConfig ellipseConfig = new EllipseConfig(beamShapeJsonObject.getJsonNumber("x").doubleValue(),
-					beamShapeJsonObject.getJsonNumber("y").doubleValue(), 
+					 beamShapeJsonObject.getJsonNumber("y").doubleValue(), 
 					new Angle(beamShapeJsonObject.getJsonNumber("angle").doubleValue() + "", Angle.DEG)); 
-
+			
+			Point2D.Double pDist = new Point2D.Double(ellipseConfig.getBeamX().getDegreeValue(), 
+					ellipseConfig.getBeamY().getDegreeValue());
+			wcstrans.imageToWorldCoords(pDist, true);
+			
+			
+			ellipseConfig.setBeamXEq(new Angle(pDist.x/step, Angle.DEG, Angle.DEG));
+			ellipseConfig.setBeamYEq(new Angle(pDist.y/step, Angle.DEG, Angle.DEG));
+			
 			
 			metaFile.setEllipseConfig(ellipseConfig);
-			
-
-			
-			metaFile.setBoresight(new Beam(jsonObject.getJsonString("boresight").getString()));
-			
 			
 			
 			JsonObject beamJsonObject = (JsonObject) jsonObject.get("beams");
@@ -85,6 +110,14 @@ public class ApsuseMetaReader {
 				Beam beam  = new Beam(name, value.toString().strip().replaceAll("\"", ""),ellipseConfig);
 				
 				if(beam.getBoresightName().equals("unset")) continue;
+				
+				
+				
+				Point2D.Double p = new Point2D.Double(beam.getRa().getDegreeValue(), beam.getDec().getDegreeValue());
+				wcstrans.worldToImageCoords(p, false);
+				beam.setRaPixel(new Angle((boresight.getRa().getDegreeValue() + p.x * step), Angle.DEG, Angle.HHMMSS));
+				beam.setDecPixel(new Angle((boresight.getDec().getDegreeValue() + p.y * step), Angle.DEG, Angle.DDMMSS));
+
 				
 				metaFile.getBeams().put(name,beam);
 				
@@ -123,6 +156,7 @@ public class ApsuseMetaReader {
 			
 			metaFile.setMaxDec(new Angle(maxDec, Angle.RAD, Angle.DDMMSS));
 			metaFile.setMinDec(new Angle(minDec, Angle.RAD, Angle.DDMMSS));
+			
 
 			return metaFile;
 		}
