0dd8f695efd217c1a383e892073c0215ec79557e
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-10-27 13:17:57 +0200

    Add reciprocal ratio of periods

--- src/main/java/application/KnownPulsarGuesser.java
+++ src/main/java/application/KnownPulsarGuesser.java
@@ -31,34 +31,35 @@ public class KnownPulsarGuesser {
 		
 		double ratio = subHarmonic? pulsarF0/candidate.getOptF0() : candidate.getOptF0()/pulsarF0;
 		
-		int intRatio = (int)Math.round(ratio);
-
-		if (intRatio < maxHarmonics && Math.abs(Math.abs(ratio - intRatio)/ratio) * 100 < percentCloseToHarmonic/100) { // Within 1% of the absolute value of the harmonic
-			
-			double fundamental = candidate.getOptF0();
-			double errFundamental = candidate.getOptF0Err();
-			
-			if(intRatio > 1)  fundamental = subHarmonic? fundamental * ratio : fundamental / ratio;
-				
-			s += ( "Harmonic:" + (subHarmonic? "1/":"") + String.format("%.4f", ratio) + "\n");	
-			
-			closeInPeriod = true;
-		}
-		
-		
-		boolean closeInDM = false;
-		
-		if( Math.abs(Math.abs(pulsarDM - candidate.getOptDM())/candidate.getOptDMErr()) < unitsCloseToDM  ){
-			
-			s += String.format("DM: %.3f \n",Math.abs(pulsarDM - candidate.getOptDM())/candidate.getOptDMErr());
-			
-		}
-		
-		boolean  closeInSpace = false;
+//		int intRatio = (int)Math.round(ratio);
+//
+//		if (intRatio < maxHarmonics && Math.abs(Math.abs(ratio - intRatio)/ratio) * 100 < percentCloseToHarmonic/100) { // Within 1% of the absolute value of the harmonic
+//			
+//			double fundamental = candidate.getOptF0();
+//			double errFundamental = candidate.getOptF0Err();
+//			
+//			if(intRatio > 1)  fundamental = subHarmonic? fundamental * ratio : fundamental / ratio;
+//				
+//			s += ( "Harmonic:" + (subHarmonic? "1/":"") + String.format("%.4f", ratio) + "\n");	
+//			
+//			closeInPeriod = true;
+//		}
+//		
+//		
+//		boolean closeInDM = false;
+//		
+//		if( Math.abs(Math.abs(pulsarDM - candidate.getOptDM())/candidate.getOptDMErr()) < unitsCloseToDM  ){
+//			
+//			s += String.format("DM: %.3f \n",Math.abs(pulsarDM - candidate.getOptDM())/candidate.getOptDMErr());
+//			
+//		}
+//		
+//		boolean  closeInSpace = false;
 		
 		s+= String.format("Angular distance: %.5f", Utilities.getAngularDistance(pulsarRA, pulsarDEC, candidate.getRa(), candidate.getDec()).getDegreeValue()) + " degrees. \n";
 		s+= String.format("Absolute DM difference: %.3f \n",Math.abs(pulsarDM - candidate.getOptDM()));
 		s+= String.format("Pulsar frequency / candidate frequency: %.6f \n",Math.abs(pulsarF0 / candidate.getOptF0()));
+		s+= String.format("Candidate frequency / pulsar frequency: %.6f \n",Math.abs(candidate.getOptF0()/pulsarF0));
 
 		return s;
 		
