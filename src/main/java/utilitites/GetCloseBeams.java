becce993def08eb9e0b37acccf24679da0dee4bd
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2022-09-19 15:00:17 +0300

    Moved some code to helpers

--- src/main/java/utilitites/GetCloseBeams.java
+++ src/main/java/utilitites/GetCloseBeams.java
@@ -11,6 +11,29 @@ import readers.ApsuseMetaReader;
 
 public class GetCloseBeams {
 	
+	public static void getCloseBeams(String arg1, String arg2) throws FileNotFoundException {
+		MetaFile metafile1 = ApsuseMetaReader.parseFile(arg1);
+		MetaFile metafile2 = ApsuseMetaReader.parseFile(arg2);
+		for (Beam b1: metafile1.getBeams().values()) {
+			
+			Map<Beam, Double> beamResponses = new HashMap<Beam, Double>();
+
+			
+			for (Beam b2: metafile2.getBeams().values()) {
+				
+				double result = MetaFile.getBeamIntersection(b1, b2);
+				beamResponses.putIfAbsent(b2, result);
+
+				
+			}
+			System.err.println(b1.getName() + " " + beamResponses.entrySet().stream()
+			.sorted(Map.Entry.comparingByValue())
+			.collect(Collectors.toList()).subList(0, 15).stream()
+			.map(f -> f.getKey().getName() + " " + f.getKey().getCoordString() +  " " + f.getValue() +"\n").collect(Collectors.toList()));
+			
+		}
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		MetaFile metafile1 = ApsuseMetaReader.parseFile(args[0]);
