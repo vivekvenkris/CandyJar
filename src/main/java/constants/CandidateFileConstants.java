8805526ed1180ec1affefa0895beffa75c2fdd61
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2023-08-08 10:39:41 +0200

    Add new pics scorers

--- src/main/java/constants/CandidateFileConstants.java
+++ src/main/java/constants/CandidateFileConstants.java
@@ -37,14 +37,21 @@ public interface CandidateFileConstants {
 	String  dist_ymw16 = "dist_ymw16";
 	String  pics_trapum_ter5 = "pics_trapum_ter5";
 	String  pics_palfa = "pics_palfa";
+	String  pics_m_LS_recall = "pics_meerkat_l_sband_combined_best_recall";
+	String  pics_pm_LS_fscore = "pics_palfa_meerkat_l_sband_best_fscore";	
 	String  png_path = "png_path";
 	String  metafile_path = "metafile_path";
 	String  filterbank_path = "filterbank_path";
 	String  candidate_tarball_path = "candidate_tarball_path"; 
 	String  tobs = "tobs";
+
+	List<String> classifierNames = Arrays.asList(new String[]{pics_palfa,pics_trapum_ter5,pics_m_LS_recall,pics_pm_LS_fscore});
+
 	
 	List<String> csvParams = Arrays.asList(new String[] {pointing_id,beam_id,beam_name,source_name,ra,dec,gl,gb,mjd_start,utc_start,
 			f0_user,f0_opt,f0_opt_err,f1_user,f1_opt,f1_opt_err,f2_opt, f2_opt_err, f2_user, acc_user,acc_opt,acc_opt_err,dm_user,dm_opt,dm_opt_err,sn_fft,
-			sn_fold,pepoch,maxdm_ymw16,dist_ymw16,pics_trapum_ter5,pics_palfa,png_path,metafile_path,filterbank_path,candidate_tarball_path, tobs});
+			sn_fold,pepoch,maxdm_ymw16,dist_ymw16,pics_trapum_ter5,pics_palfa,png_path,metafile_path,filterbank_path,candidate_tarball_path, tobs,
+		pics_palfa,pics_trapum_ter5,pics_m_LS_recall,pics_pm_LS_fscore});
+
 
 }
